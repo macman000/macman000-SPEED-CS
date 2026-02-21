320b2048ac7fcad32308c366264b1646dd6cef15
macman000 <macmellors@gmail.com>
2026-02-20 18:49:48 -0800

    edit C-5-1.py

--- C-5-1.py
+++ C-5-1.py
@@ -7,3 +7,9 @@ def sigmaid(x):
 
 def sigmoid_derivative(x):
     return x * (1 - x)
+
+X = np.array([[0, 0],
+              [0, 1],
+              [1, 0],
+              [1, 1]])
+y = np.array([[0], [1], [1], [0]])
