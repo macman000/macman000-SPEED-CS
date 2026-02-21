22696c3f487471cb2506b5c4b29f3354dd19ffd6
macman000 <macmellors@gmail.com>
2026-02-20 18:56:25 -0800

    edit C-5-1 again

--- C-5-1.py
+++ C-5-1.py
@@ -13,3 +13,16 @@ X = np.array([[0, 0],
               [1, 0],
               [1, 1]])
 y = np.array([[0], [1], [1], [0]])
+
+np.random.seed(42) # for reproducibility
+
+# 2 input neurons, 2 hidden neurons
+input_size = 2
+hidden_size = 2
+output_size = 1
+
+# Weights
+W1 = np.random.randn(input_size, hidden_size)
+b1 = np.zeros((1, hidden_size))
+W2 = np.random.randn(hidden_size, output_size)
+b2 = np.zeros((1, output_size))
